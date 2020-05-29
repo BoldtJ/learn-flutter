f9802ad92576bf931011177649a1a992c220386f
BOL_T <boldtcast@gmail.com>
2020-05-29 16:32:47 -0500

    answer chosen answer chosen answer chose

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,43 @@ import 'package:flutter/material.dart';
 //   runApp(MyApp());
 // }
 
-void main () => runApp(MyApp());
-
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  void answerQuestion(){
+    print('Answer Chosen!');
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Scaffold(
-      appBar: AppBar(title: Text('My First App'),),
-      body: Text('This is my default text'),
-    ),);
+    var questions = [
+      'What\' is your favorite color?',
+      'What\'s your favorite animal?'
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(
+          children: [
+            Text('The Question'),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: answerQuestion,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
